8ec06ee1be0e6a7dd95d02f434d8ff8939e26ce1
Daniel Mikula <daniel.mikula11@gmail.com>
2018-12-30 21:06:10 +0100

    date

--- t1/test.js
+++ t1/test.js
@@ -18,3 +18,26 @@ var myCar4 = new Car(100, "Jinx");
 
 myCar.drive(89,3);
 myCar2.logDriver();
+
+var birthday = new Date(1986, 5, 18, 15, 13, 59);
+var birthday2 = new Date(1986, 5, 18, 15, 13, 59);
+
+
+console.log(birthday.getMonth());
+
+console.log(birthday.getFullYear());
+
+console.log(birthday.getDate());
+
+console.log(birthday.getDay());
+
+console.log(birthday.getHours());
+
+console.log(birthday.getTime());
+
+if(birthday.getTime == birthday2.getTime){
+    console.log("birthdays are equal");
+} else{
+    console.log("birthdays are not equal");
+}
+
